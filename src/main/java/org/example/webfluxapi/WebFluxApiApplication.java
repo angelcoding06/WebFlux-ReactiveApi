7fb74d82092c64849ceba3f6aacd55dca97a0ac2
Angel <anvasquezge@gmail.com>
2025-06-20 18:20:12 -0500

    Commit Inicial, incio del proyecto.

---
+++ src/main/java/org/example/webfluxapi/WebFluxApiApplication.java
@@ -0,0 +1,13 @@
+package org.example.webfluxapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebFluxApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebFluxApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/webfluxapi/WebFluxApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.webfluxapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebFluxApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
